c2ed502024b8127f8f698ca559f1d9a7f1acc931
peterennis <peterennis@yahoo.com>
2019-12-30 21:16:43 -0800

    fix() use jsx comments

--- src/ae-devlist-component/ae-devlist-component.tsx
+++ src/ae-devlist-component/ae-devlist-component.tsx
@@ -57,22 +57,29 @@ export class AeDevlistComponent {
               Ionic Card
             </h1>
             <h2>
-              {/*<!-- Default Spinner -->*/}
-              <ion-spinner color="danger" />
-              {/*<!-- Lines -->*/}
-              <ion-spinner name="lines" />
-              {/*<!-- Lines Small -->*/}
-              <ion-spinner name="lines-small" />
-              {/*<!-- Dots -->*/}
-              <ion-spinner name="dots" />
-              {/*<!-- Bubbles -->*/}
-              <ion-spinner name="bubbles" />
-              {/*<!-- Circles -->*/}
-              <ion-spinner name="circles" />
-              {/*<!-- Crescent -->*/}
-              <ion-spinner name="crescent" />
-              {/*<!-- Paused Default Spinner -->*/}
-              <ion-spinner paused />
+              {/* Default Spinner */}
+              <ion-spinner></ion-spinner>
+
+              {/* Lines */}
+              <ion-spinner name="lines"></ion-spinner>
+
+              {/* Lines Small */}
+              <ion-spinner name="lines-small"></ion-spinner>
+
+              {/* Dots */}
+              <ion-spinner name="dots"></ion-spinner>
+
+              {/* Bubbles */}
+              <ion-spinner name="bubbles"></ion-spinner>
+
+              {/* Circles */}
+              <ion-spinner name="circles"></ion-spinner>
+
+              {/* Crescent */}
+              <ion-spinner name="crescent"></ion-spinner>
+
+              {/* Paused Default Spinner */}
+              <ion-spinner paused></ion-spinner>
             </h2>
           </ion-card-header>
         </ion-card>
